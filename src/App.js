c5fd208a19eb08b3b0f45af27b3b203d3a20c26d
rogeriop <pinheiroxrogerio@gmail.com>
2019-10-25 18:50:24 -0300

    Curso devReactJs #06.09: Projeto Contador com Classe ES6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contador: 0
+  }
+  constructor(props) {
+    super(props);
+    this.clear = this.clear.bind(this);
+  }
+  increment = () => {
+    this.setState({
+      contador: this.state.contador+1
+    })
+  }
+  decrement = () => {
+    this.setState({
+      contador: this.state.contador-1
+    })
+  }
+  clear () {
+    this.setState ({
+      contador: 0
+    })
+  }
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Contador: {this.state.contador}
+          <button onClick={this.increment}>+</button>
+          <button onClick={this.decrement}>-</button>
+          <button onClick={this.clear}>Clear</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
